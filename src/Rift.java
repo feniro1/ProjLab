22310e5b75aa877f610ca400213d72578753d1f0
Mate Papp <matepapp@icloud.com>
2016-03-29 13:16:17 +0200

    Rift commented

--- src/Rift.java
+++ src/Rift.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 public class Rift extends MapElement {
 
-
+    //Rift konstruktora
     public Rift() {
         System.out.println("Létrejön egy szakadék objektum.");
     }
 
 
+    //Az ezredes belelephet a szakadekba, csak meg fog halni
     public boolean stepOn(Colonel colonel) {
         System.out.println("Az ezredes ráléphet a szakadékra és meghal.");
         colonel.kill();
@@ -17,6 +18,7 @@ public class Rift extends MapElement {
     }
 
 
+    //Az ezredes leteheti a dobozt
     public boolean putDown(Box b) {
         System.out.println("Az ezredes dobhat dobozt a szakadékba.");
         return true;

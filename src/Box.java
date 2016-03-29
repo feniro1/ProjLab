dc97f8f30f8a987d5e5d7903e4bd71b678163a0a
kovacsegrikristof <kovacsegrikristof@gmail.com>
2016-03-29 02:05:39 +0200

    ZPM és Box pipa

--- src/Box.java
+++ src/Box.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Box {
 
     public Box() {
-        System.out.println("Sikerült készíteni egy új ZPM modul objektumot.");
+        System.out.println("Sikerült készíteni egy új doboz objektumot.");
     }
 
 

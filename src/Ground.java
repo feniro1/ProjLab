6ad48ddc537765a2fcc9a51b1ae58f3a00ca85d2
Mate Papp <matepapp@icloud.com>
2016-03-31 17:31:01 +0200

    Ground pipa

--- src/Ground.java
+++ src/Ground.java
@@ -9,13 +9,13 @@ public class Ground extends MapElement {
 
 
     public  Ground() {
-        System.out.println("A talaj objektum létrehozása sikeresen megtörtént.");
+        System.out.println("A talaj objektum letrehozasa sikeresen megtortent.");
     } // konstruktor a talaj objektumhoz
 
     public Ground(Box box, ZPM zpm) { // paraméteres konstruktor, mely beállít egy zpm modult és egy boxot
         this.box = box;
         this.zpm = zpm;
-        System.out.println("A talaj objektum létrehozása sikeresen megtörtént.");
+        System.out.println("A talaj objektum letrehozasa sikeresen megtortent.");
     }
 
     public boolean hasZPM() { // visszaadja, hogy a talajon van-e zpm
@@ -38,23 +38,23 @@ public class Ground extends MapElement {
 
     public void removeZPM() { // null-ra állítja a zpm paramétert
         zpm = null;
-        System.out.println("A talaj objektumon nincs többé ZPM.");
+        System.out.println("A talaj objektumon nincs tobbe ZPM.");
     }
 
-    public void removeBox() { // null-ra állítja a zpm paramétert
+    public void removeBox() { // null-ra állítja a doboz paramétert
         box = null;
-        System.out.println("A talaj objektumon nincs többé doboz.");
+        System.out.println("A talaj objektumon nincs tobbe doboz.");
     }
 
-    public void createBox() { // null-ra állítja a doboz paramétert
+    public void createBox() { // átállítja a doboz paramétert
         this.box = box;
-        System.out.println("A talajra rákerült egy doboz");
+        System.out.println("A talajra rakerult egy doboz");
 
     }
 
     public boolean stepOn(Colonel colonel) { //visszaadja, hogy az ezredes ráléphet-e a talaj objektumra
         if (!hasBox()) {
-            System.out.println("A talajon nincs doboz, így az ezredes ráléphet.");
+            System.out.println("A talajon nincs doboz, igy az ezredes ralephet.");
             return true;
         }
 
@@ -70,13 +70,13 @@ public class Ground extends MapElement {
             System.out.println("Az ezredes a dobozt sikeresen felvette.");
             return tmp;
         }
-        System.out.println("Nincs mit felvegyél, mivel nincs előtted doboz");
+        System.out.println("Nincs mit felvegyel, mivel nincs elotted doboz");
         return null;
     }
 
     public boolean putDown(Box b) { // visszaadja, hogy tehet le dobozt az ezredes és ha igen leteszi azt, és visszaad egy booleant, hogy sikerült-e
         if (hasBox()) {
-            System.out.println("A talajon már van doboz, így nem teheted le.");
+            System.out.println("A talajon mar van doboz, igy nem teheted le.");
             return false;
         } else {
             box = b;

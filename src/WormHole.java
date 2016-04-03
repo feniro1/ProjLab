17a91f8659043cade24549ed9ccaeb660d2bc4f2
Mate Papp <matepapp@icloud.com>
2016-04-03 12:25:26 +0200

    Fanni WormHole

--- src/WormHole.java
+++ src/WormHole.java
@@ -7,49 +7,86 @@ public class WormHole {
     private Portal yportal;
     private Portal bportal;
 
-    //Konstruktor. Létrehozz két portált, aminek kezdetben csak a színét beállítja, de ezek még
-    // nincsenek speciális falhoz rendelve.
+    private Portal rportal;
+    private Portal gportal;
+
+
+    //Konstruktor. Letrehoz negy portalt, aminek kezdetben csak a szinet beallitja, de ezek meg
+    // nincsenek specialis falhoz rendelve.
     public WormHole() {
         yportal = new Portal();
         bportal = new Portal();
+        rportal = new Portal();
+        gportal = new Portal();
         yportal.setColor(Color.Yellow);
         bportal.setColor(Color.Blue);
-        System.out.println("Létrejön egy féregjárat.");
-    }
-/*
-    public void setYPortal(Bullet bull) {
-        // TODO implement here
+        rportal.setColor(Color.Red);
+        gportal.setColor(Color.Green);
+        System.out.println("Letrejon egy feregjaratokat kezelo elem.");
     }
 
-    public void setBPortal(Bullet bull) {
-        // TODO implement here
-    }
-*/
-    //Sárga portálhoz tartozó setter. A fal irányát és a hozzá tartozó speciális falat kell beállítani
-    public void setYPortal(Direction dir, SpecialWall swall){
-        yportal.getSpecWall().setYPortal(null);
-        yportal.setDirection(dir);
-        yportal.setSpecWall(swall);
-        System.out.println("Létrejött egy sárga portál.");
+    //beallitja a megfelelo portalt, ha pont olyan helyen akarunk letrehozni portalt, ahol mar van, akkor nem hozza letre
+    //egyebkent pedig a kapott parameterek alapjan beallitja a megfelelo szinu portalt a megadott specwallra a megadott iranyaba
+    public void setPortal(Direction dir, SpecialWall swall, Color c){
+        if (getThisPortal(dir, swall) == null){
+            switch (c){
+                case Yellow:
+                    yportal.setSpecWall(swall);
+                    yportal.setDirection(dir);
+                    System.out.println("Sarga portal beallitva");
+                    break;
+                case Blue:
+                    bportal.setSpecWall(swall);
+                    bportal.setDirection(dir);
+                    System.out.println("Kek portal beallitva");
+                    break;
+                case Red:
+                    rportal.setSpecWall(swall);
+                    rportal.setDirection(dir);
+                    System.out.println("Piros portal beallitva");
+                    break;
+                case Green:
+                    gportal.setSpecWall(swall);
+                    gportal.setDirection(dir);
+                    System.out.println("Zold portal beallitva");
+                    break;
+                default:
+                    System.out.println("Hiba");
+                    break;
+            }
+        }
+        else
+            System.out.println("Mar letezik adott helzen portal");
+
     }
 
-    //Kék portálhoz tartozó setter. A fal irányát és a hozzá tartozó speciális falat kell beállítani
-    public void setBPortal(Direction dir, SpecialWall swall){
-        //bportal.getSpecWall().setBPortal(null);
-        bportal.setDirection(dir);
-        bportal.setSpecWall(swall);
-        System.out.println("Létrejött egy kék portál.");
+    //megadja, hogy adott specwallhoz adott iranyba letezik-e portal es ha igen azzal ter vissza, ha nincs, akkor null
+    public Portal getThisPortal(Direction dir, SpecialWall swall){
+        if (yportal.getSpecWall() == swall && yportal.getDirection() == dir)
+            return yportal;
+        else if (bportal.getSpecWall() == swall && bportal.getDirection() == dir)
+            return bportal;
+        else if (rportal.getSpecWall() == swall && rportal.getDirection() == dir)
+            return rportal;
+        else if (gportal.getSpecWall() == swall && gportal.getDirection() == dir)
+            return gportal;
+        else
+            return null;
     }
+    //megadja, hogy adott specwallhoz adott iranyba letezik-e feregjarat es ha igen visszater a masik portallal, ha nem akkor null
+    public Portal getOtherPortal(Direction dir, SpecialWall swall){
+        if (yportal.getSpecWall() == swall && yportal.getDirection() == dir && bportal.getSpecWall() != null)
+            return bportal;
+        else if (bportal.getSpecWall() == swall && bportal.getDirection() == dir && yportal.getSpecWall() != null)
+            return yportal;
+        else if (rportal.getSpecWall() == swall && rportal.getDirection() == dir && gportal.getSpecWall() != null)
+            return gportal;
+        else if (gportal.getSpecWall() == swall && gportal.getDirection() == dir && rportal.getSpecWall() != null)
+            return rportal;
+        else{
+            System.out.println("Nincs ezen a falon portal vagy nincs hozza tartozo masik portal");
+            return null;
+        }
 
-    //Portálokhoz tartozó getter
-    public Portal getYPortal() {
-        System.out.println("Visszaadja a sárga portált");
-        return yportal;
     }
-
-    public Portal getBPortal() {
-        System.out.println("Visszaadja a kék portált");
-        return bportal;
-    }
-
 }

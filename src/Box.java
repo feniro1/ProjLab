b2a3caee72e762210c6b12b43b6ee96a8d98d7ab
kovacsegrikristof <kovacsegrikristof@gmail.com>
2016-04-24 10:31:00 +0300

    box done

--- src/Box.java
+++ src/Box.java
@@ -4,10 +4,16 @@ import java.util.*;
 
 public class Box {
 
-    public Box() {
-        System.out.println("Sikerült készíteni egy új doboz objektumot.");
-    } // létrejön egy doboz objektum
+    // attributum, mely a doboz sulyat tarolja
+    private int weight;
 
+    // letrehozunk egy bizonyos sulyu doboz objektumot
+    public Box(int weight) {
+        this.weight = weight;
+    }
 
-
+    // a doboz sulyat ezzel a getterrel kerjuk le
+    public int getWeight() {
+        return weight;
+    }
 }

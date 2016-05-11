d7ebc018e0c9c9c8e1a92b95bffd293fb509d47d
Mate Papp <matepapp@icloud.com>
2016-05-11 16:54:16 +0200

    View

--- src/View.java
+++ src/View.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 
 /**
@@ -7,13 +8,27 @@ public class View {
     private MapElement firstElement;
     private int row;
     private int column;
+    private JPanel panel;
     private ArrayList<Drawable> map;
+
+    public View() {
+        JFrame window = new JFrame("Portal Game Liskovtheorem");
+        panel = new JPanel();
+
+        window.add(panel);
+
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setBounds(100, 100, 800, 800);
+        window.setVisible(true);
+    }
 
     public void drawMap(){
 
     }
 
     public void setList(MapElement fe, int r, int c){
-
+        firstElement = fe;
+        row = r;
+        column = c;
     }
 }
